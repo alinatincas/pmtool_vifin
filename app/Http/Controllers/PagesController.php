ce18418bd8f9071b0d74120461a5c9f46bb97295
Alina Tincas <alin0831@edu.eal.dk>
2019-05-09 22:38:21 +0200

    Created some pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        return view('pages.index');
+    }
+
+    public function timesheet()
+    {
+        $title = 'TIMESHEET';
+        //return view('pages.timesheet');
+        return view('pages.timesheet', compact('title'));
+    }
+}
---
+++ resources/views/pages/timesheet.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}} page</h1>
+@endsection
+
