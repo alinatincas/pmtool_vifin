4efd8a0ecd97070b18325b50a20bf54d1565a3bf
Alina Tincas <alin0831@edu.eal.dk>
2019-05-22 17:03:32 +0200

    fixed a git vulnerability for morris js package

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h1>{{$project[0]->project_name}}</h1>
-    <h3>{!!$project[0]->description!!}</h3>
-    <a href="/projects" class="btn btn-danger">Go Back</a>
+    <div>
+        <h3>{!!$project[0]->description!!}</h3>
+        <a href="/projects" class="btn btn-danger">Go Back</a>
+    </div>
+    
 @endsection
